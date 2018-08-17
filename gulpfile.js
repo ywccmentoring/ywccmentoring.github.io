55fc11a3048a830b88c5fba2efabc3f811b0797b
Emma <elr9@njit.edu>
2018-08-17 00:06:17 -0400

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -31,3 +31,14 @@ gulp.task('serve', ['sass'], function() {
 });
 
 gulp.task('default', ['js','serve']);
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
